196dd5202e3a2db194857d71f16e0d4a631f76cc
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-12-26 14:06:52 -0300

    serve qr code #1

--- main.go
+++ main.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/yeqown/go-qrcode/v2"
 	"github.com/yeqown/go-qrcode/writer/standard"
 )
 
 func main() {
-	a := Aluno{"pedro_meu_email@gmail.com", "falksjfdkaj12j31lj2"}
+	r := gin.Default()
+	r.Static("/assets", "./assets/")
+	r.GET("qr-code", func(ctx *gin.Context) {
+		a := Aluno{"pedro_meu_email@gmail.com", "falksjfdkaj12j31lj2"}
 
-	jsn, err := a.MarshalJSON()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to marshal struct with error: %v", err)
-		return
-	}
+		jsn, err := a.MarshalJSON()
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, "Failed to marshal struct with error: %v", err)
+			return
+		}
 
-	qrc, err := qrcode.New(string(jsn))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to generate QR code with error: %v", err)
-		return
-	}
+		qrc, err := qrcode.New(string(jsn))
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, "Failed to generate QR code with error: %v", err)
+			return
+		}
 
-	w, err := standard.New("./assets/qr-code.jpeg")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed create writer with: %v", err)
-		return
-	}
+		filepath := "./assets/qr-code.jpg"
+		w, err := standard.New(filepath)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, "Failed to create writer with: %v", err)
+			return
+		}
 
-	if err = qrc.Save(w); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to write QR code to file with: %v", err)
-		return
-	}
+		if err = qrc.Save(w); err != nil {
+			ctx.String(http.StatusInternalServerError, "Failed to write QR code to file with: %v", err)
+			return
+		}
 
-	fmt.Println(qrc)
+		ctx.File(filepath)
+	})
+	r.Run(":8080")
 }
